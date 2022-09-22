7363d08f9a3ab283bbdc4bfc9f5e5531e2385584
Vladimir Kamuz <v.kamuz@gmail.com>
2022-09-22 16:47:05 +0300

    Updated dump.sql

--- dump.sql
+++ dump.sql
@@ -1,23 +1,14 @@
-/*
-Navicat MySQL Data Transfer
+-- Adminer 4.7.8 MySQL dump
 
-Source Server         : openserver
-Source Server Version : 50638
-Source Host           : localhost:3306
-Source Database       : simplemvc
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-Target Server Type    : MYSQL
-Target Server Version : 50638
-File Encoding         : 65001
+DROP DATABASE IF EXISTS `simplemvc`;
+CREATE DATABASE `simplemvc` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `simplemvc`;
 
-Date: 2018-05-03 00:24:09
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for posts
--- ----------------------------
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -28,23 +19,25 @@ CREATE TABLE `posts` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of posts
--- ----------------------------
+TRUNCATE `posts`;
+INSERT INTO `posts` (`id`, `user_id`, `title`, `body`, `create_at`) VALUES
+(1,	1,	'Awesome PHP',	'This is first article',	'2022-09-22 13:40:46'),
+(2,	1,	'Simple JavaScript',	'This is the first page',	'2022-09-22 16:15:21'),
+(3,	2,	'Node.js for web artisans',	'Node.js is sever side JavaScript',	'2022-09-22 16:20:16'),
+(4,	2,	'Intro to MySQL',	'MySQL is the most popular DBMS for the PHP applications',	'2022-09-22 13:49:07');
 
--- ----------------------------
--- Table structure for users
--- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `create_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of users
--- ----------------------------
+TRUNCATE `users`;
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(1,	'Volodymyr Kamuz',	'v.kamuz@gmail.com',	'$2y$10$.32InWLNepl3uevLe9sHH.rksaPtjpKzyx4qIMeG2rDGtv4BPU.7W'),
+(2,	'Igor Kamuz',	'i.kamuz@gmail.com',	'$2y$10$f09A.dqDS5fzMQ5Ko4c.luCNYJusdZFYGBtvJ7asmEmXmWWhes/VW');
+
+-- 2022-09-22 13:43:55
