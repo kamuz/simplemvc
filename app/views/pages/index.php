e442e777048ce8922dd58bb62d4961a1c9bbabe5
Vladimir Kamuz <v.kamuz@gmail.com>
2022-09-22 16:50:47 +0300

    CSS changes

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -2,7 +2,7 @@
     <div class="jumbotron jumbotron-flud text-center">
         <div class="container">
         <h1><?php echo $data['title']; ?></h1>
-        <p class="lead"><?php echo $data['description']; ?></p>
+        <p><?php echo $data['description']; ?></p>
         </div>
     </div> 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
